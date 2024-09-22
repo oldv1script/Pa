eae655cb1f6318e78e1a7f997b3679a7654e03b2
oldv1script <oldv1script@gmail.com>
2024-09-22 22:51:06 +0530

    Update Nirajj.py

--- Nirajj.py
+++ Nirajj.py
@@ -8,10 +8,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # insert your Telegram bot token here
-bot = telebot.TeleBot('7433469650:AAFSSHu7X7SmsZXTbaDH6DH-Ag_pOeIVXUg')
+bot = telebot.TeleBot('7439726303:AAFiyPRm_RWwBOVVPj7tpT-18HXYyzgQhQ0')
 
 # Admin user IDs
-admin_id = ["6701861445"]
+admin_id = ["969452944"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -48,7 +48,7 @@ allowed_user_ids = read_users()
 
 # Function to log command to the file
 def log_command(user_id, target, port, time):
-    admin_id = ["6701861445"]
+    admin_id = ["969452944"]
     user_info = bot.get_chat(user_id)
     if user_info.username:
         username = "@" + user_info.username
